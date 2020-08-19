3d8125bf46f6d16fa6df83f2faa27817a6fe6b1c
Jon Penton <jon@penton.site>
2020-08-19 09:05:41 -0400

    Calulcate total time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,48 @@
+import { Field, Formik } from 'formik'
+
 function HomePage() {
-  return <p>Home page</p>;
+  return (
+    <Formik initialValues={{ baseWork: 0, totalWork: 8 }} onSubmit={() => null}>
+      {({ values }) => {
+        const [hours, minutes] = calculateTotalTime(values)
+
+        return (
+          <>
+            <label>
+              Base Work
+              <br />
+              <Field name="baseWork" type="number" />
+            </label>
+            <br />
+            <label>
+              Total Work
+              <br />
+              <Field name="totalWork" type="number" />
+            </label>
+            <p>
+              Total time: {hours} hours {minutes} minutes
+            </p>
+          </>
+        )
+      }}
+    </Formik>
+  )
 }
 
-export default HomePage;
+function calculateTotalTime({
+  baseWork,
+  totalWork,
+}: Record<string, number>): number[] {
+  const baseWorkMinutes = baseWork * 60
+  const totalWorkMinutes = totalWork * 60
+  const pomodoroWork = totalWorkMinutes - baseWorkMinutes // 285
+  const totalPomodoroTime = Math.ceil(pomodoroWork / 25) * 30 // 360
+  const totalMinutes = totalPomodoroTime + baseWorkMinutes // 495
+  const hours = Math.floor(totalMinutes / 60)
+  const minutes = totalMinutes % 60
+  const numberOf15Minutes = Math.floor(minutes / 15)
+
+  return [hours, numberOf15Minutes * 15]
+}
+
+export default HomePage
