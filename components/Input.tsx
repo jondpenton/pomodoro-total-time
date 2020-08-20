ac0e315ba7a15c55c3f8e47395aa3060e42429ed
Jon Penton <jon@penton.site>
2020-08-19 23:11:58 -0400

    Dark theme

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,18 +5,20 @@ export function Input({ label, description, name, ...rest }: any) {
     <>
       <div className="flex justify-between">
         <label
-          className="block text-sm font-medium leading-5 text-gray-700"
+          className="block text-sm font-medium leading-5 text-cool-gray-300"
           htmlFor={name}
         >
           {label}
         </label>
         {description && (
-          <span className="text-sm leading-5 text-gray-500">{description}</span>
+          <span className="text-sm leading-5 text-cool-gray-400">
+            {description}
+          </span>
         )}
       </div>
       <div className="mt-1 relative rounded-md shadow-sm">
         <Field
-          className="form-input block w-full sm:text-sm sm:leading-5"
+          className="focus:outline-none focus:border-indigo-500 focus:border-4 focus:shadow-focus appearance-none block w-full bg-cool-gray-700 border-cool-gray-500 border rounded-md px-3 py-2 text-lg leading-normal sm:text-sm sm:leading-5 text-cool-gray-300"
           name={name}
           {...rest}
         />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   plugins: [require('@tailwindcss/ui')],
   theme: {
     extend: {
+      boxShadow: {
+        ...defaultTheme.boxShadow,
+        focus: '0 0 0 3px rgba(141, 162, 251, 0.45)',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
